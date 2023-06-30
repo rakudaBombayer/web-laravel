596dd7546e472e56967ea16f6d8aa53982338a96
rakudaBombayer <113893608+rakudaBombayer@users.noreply.github.com>
2023-06-30 16:49:36 +0900

    108

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -20,6 +20,7 @@
                           <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">氏名</th>
                           <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">件名</th>
                           <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">登録日</th>
+                          <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">詳細</th>
                           
                         </tr>
                       </thead>
@@ -30,6 +31,7 @@
                           <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->name }}</td>
                           <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->title }}</td>
                           <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900">{{ $contact->created_at }}</td>
+                          <td class="border-t-2 border-gray-200 px-4 py-3 text-lg text-gray-900"><a class="text-blue-500" href="{{ route('contacts.show', ['id' => $contact->id ])}}">詳細を見る</a></td>
                         </tr>
                         @endforeach
                       </tbody>
